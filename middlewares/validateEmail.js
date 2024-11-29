8dc3c0433a03f15a217041102105a3d15ead5ed4
Francesca <guizarfrancesca7@gmail.com>
2024-11-29 18:11:09 -0300

    agregada la verficacion para emails: gmail hotmail y outlook

--- middlewares/validateEmail.js
+++ middlewares/validateEmail.js
@@ -1,13 +1,13 @@
-export function validateGmail(req, res, next) {
-    const { email } = req.body;
-  
-    // Expresión regular para validar Gmail
-    const gmailRegex = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
-  
-    if (!email || !gmailRegex.test(email)) {
-      return res.status(400).json({ error: 'El correo electrónico debe ser un Gmail válido' });
-    }
-  
-    // Si es válido, continúa con el siguiente middleware o controlador
-    next();
+export function validateEmails(req, res, next) {
+  const { email } = req.body;
+
+  // Expresión regular para validar Gmail, Hotmail y Outlook
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@(gmail\.com|hotmail\.com|outlook\.com)$/;
+
+  if (!email || !emailRegex.test(email)) {
+    return res.status(400).json({ error: 'El correo electrónico debe ser un Gmail, Hotmail o Outlook válido' });
   }
+
+  // Si es válido, continúa con el siguiente middleware o controlador
+  next();
+}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,13 +5,13 @@ import {
   updateUser,
   deleteUser,
 } from '../controllers/userController.js';
-import { validateGmail } from '../middlewares/validateEmail.js';
+import { validateEmails } from '../middlewares/validateEmail.js';
 
 const router = express.Router();
 
 router.get('/user', getAllUsers);        // Obtener todos los usuarios
-router.post('/user', validateGmail, createUser);        // Crear un usuario
-router.put('/user/:id',validateGmail, updateUser);      // Actualizar un usuario por ID
+router.post('/user', validateEmails, createUser);        // Crear un usuario
+router.put('/user/:id',validateEmails, updateUser);      // Actualizar un usuario por ID
 router.delete('/user/:id', deleteUser);  // Eliminar un usuario por ID
 
 export default router;
