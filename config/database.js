b6c5ed0403c5831cc4c3516d12de8cb5ad172f14
Francesca <guizarfrancesca7@gmail.com>
2024-11-29 17:42:47 -0300

    modificacion del port

--- config/database.js
+++ config/database.js
@@ -2,15 +2,18 @@ import { createPool } from 'mysql2/promise';
 import { config } from 'dotenv';
 
 config();
+
+const PUERTO_MYSQL = 3306;
+
 const pool = createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+  port: process.env.DB_PORT || PUERTO_MYSQL,
 });
+
+console.log(process.env.DB_PORT?"true":"false");
+
 
 export default pool;
